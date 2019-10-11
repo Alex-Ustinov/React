eb7f907c269a38469d3fb9119f0e3b21183d45d2
Alex-Ustinov <39276863+Alex-Ustinov@users.noreply.github.com>
2019-10-11 19:59:52 +0300

    Update TodoItem.js

--- my-app/src/Todo/TodoItem.js
+++ my-app/src/Todo/TodoItem.js
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types';
 import Context from '../context';
 import { OutlinedInput ,Hidden , InputAdornment , MenuItem, TextField} from '@material-ui/core';
 import NumberFormat from 'react-number-format';
-/*
-const styles = theme => ({
-    notchedOutline: {
-        borderWidth: "1px",
-        borderColor: "yellow !important"
-    }
-});*/
+
 
 const styles ={
     li:{
@@ -22,22 +16,7 @@ const styles ={
         marginBottom: '.5rem'
     },
 }
-/*
-    input:{
-        marginRight: '1rem',
-        size: '150px',
-    },
-    rm: {
-    background: 'red',
-    borderRadius: "50%",
-    collor: '#fff',
-    border: "none",
-    },
-    done: {
-        textDecoration: "line-through",
-    }
-}
-*/
+
 const suggestions = [
     { label: 'Саратов' },
     { label: 'Москва' },
@@ -46,14 +25,9 @@ const suggestions = [
 ];
 function TodoItem({todo,ind, onChange}) {
     const {removeTodo} = useContext(Context)
-    console.log(todo)
-    const clases = []
-    if(todo.completed){
-        clases.push('done')
-    }
     return(
         <li style={styles.li}>
-            < NumberFormat  customInput = { TextField }  format = {todo.format} variant="outlined" label={todo.label} fullWidth = "true" required={todo.hiden} error={todo.error}  type= {todo.type} placeholder={todo.title} onChange = {(event)=>onChange(todo.id , event.target.value)} />
+            < NumberFormat  customInput = { TextField }  format = {todo.format} variant="outlined" label={todo.label} fullWidth = "true"  error={todo.error} placeholder={todo.title} onChange = {(event)=>onChange(todo.id , event.target.value)} />
         </li>
     )
 }
