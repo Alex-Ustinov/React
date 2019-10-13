61f53c7830b5a5e7d5a0b65d1990ed0f28217341
Alex-Ustinov <39276863+Alex-Ustinov@users.noreply.github.com>
2019-10-14 02:09:32 +0300

    Update AddTodo.js

--- my-app/src/Todo/AddTodo.js
+++ my-app/src/Todo/AddTodo.js
@@ -1,35 +1,44 @@
-import React , {useState}from 'react';
+import React , {useState , createRef ,useContext }from 'react';
 import TodoItem from "./TodoItem";
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button';
-import purple from '@material-ui/core/colors/purple';
-import Card from '@material-ui/core/Card';
+import Context from '../context'
 
 
-function AddTodo({onCreate}){
-
-    const primary = purple;
-
-    const [value, setValue] = React.useState('')
-
+function AddTodo(props){
+    const getAllData = useContext(Context)
+    let answer = false
     function submitHandler (event){
+
         event.preventDefault()
-        if(value.trim()){
-            onCreate(value)
-            setValue('')
+        getAllData.todos.concat(props.fio)
+        for(var g = 0; g<getAllData.todos.length;g++){
+            if(getAllData.todos[g].value != ''){
+                answer = true
+            }else{
+                answer = false
+                break
+            }
         }
+        if(!answer) return props.response(answer)
+        for(var f = 0; f<props.dataDestination.length;f++){
+            for (var k in props.dataDestination[f]){
+                if(props.dataDestination[f]['text'] != '' && props.dataDestination[f]['textTown'] != ''){
+                    answer = true
+                }else{
+                    answer = false
+                    break
+                }
+            }
+        }
+        return props.response(answer)
     }
 
     return(
-        <Card >
-            <form style = {{marginBottom: '1rem'}} onSubmit={submitHandler}>
+            <form onSubmit={submitHandler}>
                 <Button size = "large" variant="contained" color="secondary" type="submit">Далее</Button>
             </form>
-        </Card>
     )
 }
 
-AddTodo.propType = {
-    onCreate: PropTypes.func.isRequired
-}
 export default AddTodo
